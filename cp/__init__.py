534c73b7cee70c4a5e1ce8370107f176134e9059
Eric Ma <ericmajinglong@gmail.com>
2019-08-27 21:52:31 -0400

    added branch as option

--- cp/__init__.py
+++ cp/__init__.py
@@ -7,23 +7,31 @@ import os
 
 @click.command()
 @click.option(
+    "--branch",
+    "-b",
+    default="master",
+    type=str,
+    help="Branch to pull. Defaults to 'master'.",
+)
+@click.option(
     "--interval",
-    prompt="Interval between git pulls (seconds)",
+    "-i",
     help="Interval between git pulls.",
     default=60,
     type=int,
 )
 @click.option(
-    "--hide-output",
-    prompt="Do you want to hide output?",
+    "--hide/--no-hide",
+    "-h/-nh",
     help="Whether or not to hide the output from the tool.",
-    type=bool,
     default=False,
 )
-def main(interval: int = 60, hide_output: bool = False):
+def main(branch: str = "master", interval: int = 60, hide: bool = False):
     logging.info(f"Currently in directory {os.getcwd()}.")
+    logging.info(f"Checking out {branch}...")
+    subprocess.run(f"git checkout {branch}".split(), capture_output=hide)
     while True:
-        subprocess.run("git pull".split(), capture_output=hide_output)
+        subprocess.run("git pull".split(), capture_output=hide)
         sleep(interval)
 
 
